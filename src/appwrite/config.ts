292dcb0d5c7a334de4e9d0bb0584df49d50497a8
swazy <crazyswazy87@gmail.com>
2026-08-17 00:23:15 -0700

    tingii

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -3,8 +3,8 @@ import { Client, Databases } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint('https://sgp.cloud.appwrite.io/v1')
+  .setProject('695fa78500123148c6ff');
 
 export const databases = new Databases(client);
 
