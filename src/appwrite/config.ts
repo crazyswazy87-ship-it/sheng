df88278207e6a07387532007a93b3002cdafc219
swazy <crazyswazy87@gmail.com>
2026-08-30 23:35:42 -0700

    env manenoz

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -3,7 +3,7 @@ import { Client, Databases } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setEndpoint('https://sgp.cloud.appwrite.io/v1')
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const databases = new Databases(client);
