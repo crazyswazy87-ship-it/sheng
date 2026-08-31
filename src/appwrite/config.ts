3f675887459cdd95ff9ee4b18d8ddb6315caf67e
swazy <crazyswazy87@gmail.com>
2026-08-30 23:49:44 -0700

    namba uno

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -3,8 +3,8 @@ import { Client, Databases } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint('https://sgp.cloud.appwrite.io/v1')
-  .setProject('695fa78500123148c6ff');
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const databases = new Databases(client);
 
@@ -18,3 +18,10 @@ export const SHENG_COLLECTION_ID =
 
 export const SHENGTEZO_COLLECTION_ID =
   import.meta.env.VITE_APPWRITE_SHENGTEZO_COLLECTION_ID;
+
+  console.log("PROJECT:", import.meta.env.VITE_APPWRITE_PROJECT_ID);
+console.log("DATABASE:", import.meta.env.VITE_APPWRITE_DATABASE_ID);
+console.log(
+  "SHENG:",
+  import.meta.env.VITE_APPWRITE_SHENG_COLLECTION_ID
+);
